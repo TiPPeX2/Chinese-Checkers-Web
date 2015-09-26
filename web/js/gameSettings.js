94d842c917df31699ac96985ab4cfc51f458b4fe
Tamir <Tamir@192.168.1.4>
2015-09-26 14:44:05 +0300

    dima

--- web/js/gameSettings.js
+++ web/js/gameSettings.js
@@ -1,2 +1,44 @@
+var gameSettings = {
+    playerNumber: 2,
+    playerName: "",
+    howManyHumans: 1
+};
+
+
+function playersNumberChanged(){
+    $('#humansNumber').empty();
+    for(var i = 1; i <= gameSettings.playerNumber; i++){
+        $("#humansNumber").append($('<option>',{
+            value: i,
+            text: i
+        }));
+    }
+}
+
+$(function(){
+    
+    $("#playersNumber").change(function(){
+    gameSettings.playerNumber = $(this).val();
+    playersNumberChanged();
+    });
+
+    $("#humansNumber").change(function(){
+        gameSettings.howManyHumans = $(this).val();
+    });
+
+    playersNumberChanged();
+    
+    $("#createGameBtn").submit(function(){
+        $.ajax({
+            url: this.action,
+            success: function(data) {
+                alert(data);
+            },
+            error: function(error) {
+                alert("error");
+            }
+        });
+    });
+});
 
 
